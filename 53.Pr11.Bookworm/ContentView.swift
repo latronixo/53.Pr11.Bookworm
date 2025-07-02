77546eff790a4743bffae9812972e4caf80bc415
Valentin <latronixo@gmail.com>
2025-07-02 19:22:22 +0300

    Initial Commit

---
+++ 53.Pr11.Bookworm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  53.Pr11.Bookworm
+//
+//  Created by Валентин on 02.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 53.Pr11.Bookworm/_3_Pr11_BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  _3_Pr11_BookwormApp.swift
+//  53.Pr11.Bookworm
+//
+//  Created by Валентин on 02.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct _3_Pr11_BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
