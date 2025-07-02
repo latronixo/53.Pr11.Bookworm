f7a6f6abbd7176527d62f3f8f1ed76d789982cf7
Valentin <latronixo@gmail.com>
2025-07-02 20:05:03 +0300

    1/9 Демонстрация, что если значение меняется внутри класса, и планируется чтобы оно менялось везде, то нужно использовать внутри структуры @Binding, а не @State

--- 53.Pr11.Bookworm/ContentView.swift
+++ 53.Pr11.Bookworm/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct PushButton: View {
     let title: String
-    @State var isOn: Bool
+    @Binding var isOn: Bool
     
     var onColors = [Color.red, Color.yellow]
     var offColors = [Color(white: 0.6), Color(white: 0.4)]
@@ -30,7 +30,7 @@ struct ContentView: View {
     @State private var rememberMe = false
     
     var body: some View {
-        PushButton(title: "Remeber Me", isOn: rememberMe)
+        PushButton(title: "Remeber Me", isOn: $rememberMe)
         Text(rememberMe ? "On" : "Off")
     }
 }
