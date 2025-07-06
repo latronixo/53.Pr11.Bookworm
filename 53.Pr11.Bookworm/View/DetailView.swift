cc9c922d943f29ad54c054c7b27f4791689e971a
Valentin <latronixo@gmail.com>
2025-07-06 21:09:48 +0300

    7/10 Добавил в Assets картинки жанров, сделал экран DetailView и переход на него при клике на строку.с книгой в List на главном экране

---
+++ 53.Pr11.Bookworm/View/DetailView.swift
@@ -0,0 +1,58 @@
+//
+//  DetailView.swift
+//  53.Pr11.Bookworm
+//
+//  Created by Валентин on 03.07.2025.
+//
+
+import SwiftData
+import SwiftUI
+
+struct DetailView: View {
+    let book: Book
+    
+    var body: some View {
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(book.genre.uppercased())
+                    .fontWeight(.black)
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                    .offset(x: -5, y: -5)
+            }
+            
+            Text(book.author)
+                .font(.title)
+                .foregroundStyle(.secondary)
+            
+            Text(book.review)
+                .padding()
+            
+            RatingView(rating: .constant(book.rating))
+                .font(.largeTitle)
+        }
+        .navigationTitle(book.title)
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
+    
+    }
+}
+
+#Preview {
+    do {
+         let config = ModelConfiguration(isStoredInMemoryOnly: true)//конфигурация модели будет храниться только в памяти
+         let container = try ModelContainer(for: Book.self, configurations: config)//создадим новый контейнер, используя модель для нашей книги
+         let example = Book(title: "Test Book", author: "Test Author", genre: "Fantasy", review: "This was a great book; I really enjoyed it.", rating: 4)//создадим пример книги
+
+         return DetailView(book: example)
+             .modelContainer(container)
+     } catch {
+         return Text("Failed to create preview: \(error.localizedDescription)")
+     }
+}
